dc47bb97a9ada3182dd01f47ef0293b30a193491
DIR\marco.accardi <accardi.marco@gmail.com>
2017-11-08 09:08:25 +0100

    primo codice per grafici

---
+++ console/platform/graph/test.php
@@ -0,0 +1,82 @@
+<?php
+//index.php
+$connect = mysqli_connect("localhost", "root", "", "fd3-Hm!Y");
+$query = '
+SELECT sensors_temperature_data, 
+UNIX_TIMESTAMP(CONCAT_WS(" ", sensors_data_date, sensors_data_time)) AS datetime 
+FROM tbl_sensors_data 
+ORDER BY sensors_data_date DESC, sensors_data_time DESC
+';
+$result = mysqli_query($connect, $query);
+$rows = array();
+$table = array();
+
+$table['cols'] = array(
+ array(
+  'label' => 'Date Time', 
+  'type' => 'datetime'
+ ),
+ array(
+  'label' => 'Temperature (°C)', 
+  'type' => 'number'
+ )
+);
+
+while($row = mysqli_fetch_array($result))
+{
+ $sub_array = array();
+ $datetime = explode(".", $row["datetime"]);
+ $sub_array[] =  array(
+      "v" => 'Date(' . $datetime[0] . '000)'
+     );
+ $sub_array[] =  array(
+      "v" => $row["sensors_temperature_data"]
+     );
+ $rows[] =  array(
+     "c" => $sub_array
+    );
+}
+$table['rows'] = $rows;
+$jsonTable = json_encode($table);
+
+?>
+
+
+<html>
+ <head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script type="text/javascript">
+   google.charts.load('current', {'packages':['corechart']});
+   google.charts.setOnLoadCallback(drawChart);
+   function drawChart()
+   {
+    var data = new google.visualization.DataTable(<?php echo $jsonTable; ?>);
+
+    var options = {
+     title:'Sensors Data',
+     legend:{position:'bottom'},
+     chartArea:{width:'95%', height:'65%'}
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('line_chart'));
+
+    chart.draw(data, options);
+   }
+  </script>
+  <style>
+  .page-wrapper
+  {
+   width:1000px;
+   margin:0 auto;
+  }
+  </style>
+ </head>  
+ <body>
+  <div class="page-wrapper">
+   <br />
+   <h2 align="center">Display Google Line Chart with JSON PHP & Mysql</h2>
+   <div id="line_chart" style="width: 100%; height: 500px"></div>
+  </div>
+ </body>
+</html>
